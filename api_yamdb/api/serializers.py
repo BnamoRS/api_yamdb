bafa4b92e133e57aacc3e6e4d4b4b86cf2ffe3a0
BnamoRS <rbutskih@mail.ru>
2022-05-13 15:38:16 +0300

    pytest пройден

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -8,6 +8,9 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
 
     class Meta:
         model = User
